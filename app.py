b4af20a2c11adcc6260d9a188229afcce65ae794
krish puneet mehrrotra <tt1251961@iitd.ac.in>
2025-09-21 14:47:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -526,5 +526,6 @@ def serve_frontend():
     return send_from_directory(app.static_folder, 'index.html')
 
 # --- Run the App ---
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+#if __name__ == '__main__':
+
+   # app.run(host='0.0.0.0', port=5000, debug=False)
